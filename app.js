2b2fe434acb7650a53dbb50931b7ee048b9ad81b
Lee Chun Hao <e1157468@u.nus.edu>
2024-09-25 12:08:11 +0000

    debug

--- app.js
+++ app.js
@@ -156,3 +156,18 @@ createApp({
         }
     }
 }).mount('#app')
+
+const backToTopButton = document.getElementById("backToTop");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+backToTopButton.onclick = function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
