a27f463674ee97388b2c61dc39ce66533c05572e
Chun Hao <chunhao.lee@u.nus.edu>
2025-03-15 19:31:13 +0800

    Modify GitHub base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/0x4F776C.github.io/',
+  base: '/',
   define: {
     'window.GITHUB_TOKEN': JSON.stringify(process.env.GITHUB_TOKEN || '')
   }
